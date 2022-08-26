20f1caaa2e872b55790a951b58bb28b205ba6655
agusvillarreal <80488773+agusvillarreal@users.noreply.github.com>
2022-08-26 10:41:48 -0500

    Update rock-paper-scissors-game.py

--- rock-paper-scissors-game.py
+++ rock-paper-scissors-game.py
@@ -1,3 +1,4 @@
+import random
 rock = '''
     _______
 ---'   ____)
@@ -25,26 +26,26 @@ scissors = '''
 ---.__(___)
 '''
 
-#Write your code below this line 👇
-import random
 game_images = [rock, paper, scissors]
 user = int(input('What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n'))
 print('Your chose:')
-print(game_images[user])
-
-computer_choice = random.randint(0, 2)
-print("Computer chose:")
-print(game_images[computer_choice])
-
 if user >= 3 or user < 0:
     print('Invalid number, you lose')
-elif user == 0 and computer_choice == 2:
-    print('You win')
-elif computer_choice == 0 and user == 2:
-    print('You lose')
-elif computer_choice > user:
-    print('You lose')
-elif user > computer_choice:
-    print('You win')
-elif computer_choice == user:
-    print('Draw')
+else:
+    print(game_images[user])
+
+    computer_choice = random.randint(0, 2)
+    print("Computer chose:")
+    print(game_images[computer_choice])
+
+
+    elif user == 0 and computer_choice == 2:
+        print('You win')
+    elif computer_choice == 0 and user == 2:
+        print('You lose')
+    elif computer_choice > user:
+        print('You lose')
+    elif user > computer_choice:
+        print('You win')
+    elif computer_choice == user:
+        print('Draw')
